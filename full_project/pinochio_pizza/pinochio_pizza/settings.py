21e97bc79520dc9a87daed421cc22e348dbccb93
Michael <mikhailyurchenko2601@gmail.com>
2020-09-06 23:54:04 +0300

    Update settings.py
    Security

--- full_project/pinochio_pizza/pinochio_pizza/settings.py
+++ full_project/pinochio_pizza/pinochio_pizza/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = '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' # this is exactly the value 'apikey'
+SENDGRID_API_KEY = '' # this is exactly the value 'apikey'
 SENDGRID_SANDBOX_MODE_IN_DEBUG=False
 
 # echo to stdout or any other file-like object that is passed to the backend via the stream kwarg.
