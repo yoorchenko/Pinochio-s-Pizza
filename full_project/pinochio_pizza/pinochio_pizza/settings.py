f1c0d46858b82c63e72c3461e8e4afea6107a476
Michael <mikahilyurchenko2601@gmail.com>
2020-09-22 20:23:22 +0300

    Database for tests

--- full_project/pinochio_pizza/pinochio_pizza/settings.py
+++ full_project/pinochio_pizza/pinochio_pizza/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -84,6 +85,7 @@ WSGI_APPLICATION = 'pinochio_pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -94,6 +96,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
 
 # DATABASES = {
 #     'default': {
